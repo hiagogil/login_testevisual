2f0f6eae62368f0eec33a3646fb63ec7d4f36511
Hiago <hiagoqagil@gmail.com>
2024-07-11 16:22:07 -0300

    correção ci

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8080/visual_testing.html')
-    cy.get('#log').type('').should('have.value','HiagoGil')
+    cy.get('#log').type('hiagos').should('have.value','HiagoGil')
     cy.get('#senha').type('Teste12345').should('have.value',  'Teste12345')
     cy.get('#ok').click()
 
--- pixeleye.config.js
+++ pixeleye.config.js
@@ -1,6 +1,6 @@
 /** @type {import('pixeleye').Config} */
 const config = {
-    token: "pxi__xIsXX-o2PMkbL67dWk8pXetAO10c6W4L:4L1M8MUHwnuDCFd9NSpon",
+    token: "pxi__h9rdxQPSlBqtaoy5YkKLEb04CBTPqNrk:qJRYv4Gx1XU_37tzeFd9I",
     // or
     // token: process.env.PIXELEYE_PROJECT_TOKEN,
     // ...
