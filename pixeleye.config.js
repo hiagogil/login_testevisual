830aa73bcd658ad2e97622e0e91288b9dbc04395
Hiago <hiagoqagil@gmail.com>
2024-07-11 16:17:25 -0300

    first commit

--- pixeleye.config.js
+++ pixeleye.config.js
@@ -1,6 +1,6 @@
 /** @type {import('pixeleye').Config} */
 const config = {
-    token: "pxi__SeHG9N2zQUGIAvwf-OruZL4hMLPYdry8:c3c1YVE8xQ-L_hcYKBIpn",
+    token: "pxi__xIsXX-o2PMkbL67dWk8pXetAO10c6W4L:4L1M8MUHwnuDCFd9NSpon",
     // or
     // token: process.env.PIXELEYE_PROJECT_TOKEN,
     // ...
